a898204ac4ae206a9ca490c13a2d204933acb1a9
angelobaricante <angelobaricante@gmail.com>
2024-11-30 20:53:52 +0800

    remove some unuse variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
 import { GoToTopButton } from "@/components/GoToTopButton";
 import BinaryBackground from "@/components/BinaryBackground";
 import { ThemeProvider } from '@/components/ThemeProvider'
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Huffmen Visualizer",
